2275178d2f5f610c71805ff376b02aa208596cef
TomasBabkineDiCaprio <60266939+TomasBabkine-DiCaprio@users.noreply.github.com>
2022-11-19 13:55:42 +0100

    Removed DS_Store files from repo

--- flatten.py
+++ flatten.py
@@ -1,7 +1,8 @@
 import sys
 import json
 
-# Returns a flattened JSON object
+
+# Returns a flattened dict object
 # Params:
 #   - jObject: dictionary
 #   - key: string, used to construct key when the JSON object is nested. Default: empty string
@@ -18,7 +19,7 @@ def flatten(jObject, key = ""):
             # recursive call to flatten and then append the result to the object currently being built
             flattened = flattened | flatten(jObject[i], i)
         
-        # else if it's a simple key - value pair
+        # if it's a simple key - value pair
         else:
 
             # build the key for this value
@@ -33,11 +34,17 @@ def flatten(jObject, key = ""):
 
 # this function transforms True to true
 # Take input using stdin
-files = sys.argv[1:]
-for f in range(len(sys.argv[1:])):
-    print(f)
-    with open(files[f]) as user_file:
-        file_contents = user_file.read()
-    
-    loaded = json.loads(file_contents)
-    print(flatten(loaded))
+if __name__ == '__main__':
+    files = sys.argv[1:]
+    for f in range(len(sys.argv[1:])):
+        print(f)
+        with open(files[f]) as user_file:
+            file_contents = user_file.read()
+        
+        loadedJSON = json.loads(file_contents)
+        print(flatten(loadedJSON))
+
+
+# mention python version
+# mention JSON conversion
+# convert to JSON Object, not dict
