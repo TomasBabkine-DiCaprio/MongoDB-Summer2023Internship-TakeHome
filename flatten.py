a66d3bf9e68bf071caa7cb1c81ac05e906babd6d
TomasBabkineDiCaprio <60266939+TomasBabkine-DiCaprio@users.noreply.github.com>
2022-11-19 16:30:52 +0100

    Added multiple nested objects support

--- flatten.py
+++ flatten.py
@@ -1,7 +1,6 @@
 import sys
 import json
 
-
 # Returns a flattened dict object
 # Params:
 #   - jObject: dictionary
@@ -12,37 +11,46 @@ def flatten(jObject, key = ""):
 
     # iterate over every key value pair in the object
     for i in jObject:
-        
+
+        # build the key for this value by appending previous key to current key
+        if key: instanceKey = key + "." + i
+        else: instanceKey = i
+
         # if value stored at i is a dict (i.e. it's a nested JSON object)
         if isinstance(jObject[i], dict):
 
             # recursive call to flatten and then append the result to the object currently being built
-            flattened = flattened | flatten(jObject[i], i)
+            flattened = flattened | flatten(jObject[i], instanceKey)
         
-        # if it's a simple key - value pair
+        # if it's a simple key-value pair
         else:
-
-            # build the key for this value
-            if key: instanceKey = key + "." + i
-            else: instanceKey = i
-
-            # append key-value pair to 
+            # append key-value pair to the object currently being built
             flattened[instanceKey] = jObject[i]
 
     return flattened
 
 
-# this function transforms True to true
-# Take input using stdin
 if __name__ == '__main__':
     files = sys.argv[1:]
+
+    # for each JSON file to flatten
     for f in range(len(sys.argv[1:])):
-        print(f)
+
         with open(files[f]) as user_file:
             file_contents = user_file.read()
         
+        # load JSON
         loadedJSON = json.loads(file_contents)
-        print(flatten(loadedJSON))
+
+        # flatten this object
+        flattenedDict = flatten(loadedJSON)
+
+        # parse path to file for output file name
+        fileName = files[f].split("/")
+
+        # output the flattened JSON object to the outputs folder as a .json file
+        with open(f"./outputs/{fileName[-1]}", "w") as outfile:
+            json.dump(flattenedDict, outfile)
 
 
 # mention python version
